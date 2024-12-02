b7d554a36575c47e7cbdc9f2e36f4d6386f6214f
circamanuel <manu4126@gmail.com>
2024-12-02 23:09:36 +0100

    first code upload

---
+++ src/dev/Ipa/Main.java
@@ -0,0 +1,31 @@
+package dev.Ipa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+record GroceryItem(String name, String type, int count) {
+
+    public GroceryItem(String name){
+        this(name, "Dairy", 1);
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GroceryItem[] groceryArray = new GroceryItem[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+        System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList objectList = new ArrayList();
+        objectList.add(new GroceryItem("Butter"));
+        objectList.add("yogurt");
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("butter"));
+
+    }
+}
